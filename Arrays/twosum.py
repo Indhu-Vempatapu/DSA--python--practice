7d0e6da3fdcc7e306356cd3ede213a249f2729e0
Indhu Vempatapu <indhuvempatapu@gmail.com>
2026-03-12 22:39:09 +0530

    Arrays-TwoSum
    Added example input and output comments to clarify usage.

--- Arrays/twosum.py
+++ Arrays/twosum.py
@@ -10,3 +10,6 @@ def twoSum(nums, target):
 nums = list(map(int, input().split()))
 target = int(input())
 print(twoSum(nums, target))
+#Example 
+#Input: nums= [2,7,11,15], target=9
+#Output: [0,1]
