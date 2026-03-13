9144d9954136bdc171f3a51055984012273bf97f
Indhu Vempatapu <indhuvempatapu@gmail.com>
2026-03-13 23:23:25 +0530

    Arrays-Merge Intervals
    Implement mergeIntervals function to merge overlapping intervals.

---
+++ Arrays/Merge Intervals.py
@@ -0,0 +1,21 @@
+def mergeIntervals(intervals):
+  intervals.sort(key=lambda x:x[0])
+  merged = []
+  for interval in intervals:
+    if not merged or merged[-1][1]<interval[0]:
+      merged.append(interval)
+    else:
+      merged[-1][1]=max(merged[-1][1],interval[1])
+  return merged
+  
+n = int(input("enter number of intervals: "))
+intervals = []
+for i in range(n):
+  start , end = map(int, input().split())
+  intervals.append([start, end])
+
+if n<=1:
+  print(intervals)
+  exit()
+else:
+  print(mergeIntervals(intervals))              
