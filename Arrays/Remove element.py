893e7233450b505333c48fdf0826715472f14bff
Indhu Vempatapu <indhuvempatapu@gmail.com>
2026-04-29 20:59:50 +0530

    in-place modification
    Implement function to remove specified element from list.

---
+++ Arrays/Remove element.py
@@ -0,0 +1,14 @@
+def removeElement(nums):
+  if not nums:
+     return 0
+  j=0
+  for i in range(len(nums)):
+    if nums[i]!=val:
+      nums[j] = nums[i]
+      j+=1
+  return j
+nums = list(map(int,input().split()))
+print(removeElement(nums))
+
+#Example input: nums = [3,2,2,3], val = 3
+#Output: [2,2] (the remaining elements are not important so we just leave them like [2,2,_,_])
