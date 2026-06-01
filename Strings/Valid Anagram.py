ec5e073cd765ae7b3200e93ce22ffbb901afeee1
Indhu Vempatapu <indhuvempatapu@gmail.com>
2026-06-01 22:42:42 +0530

    Strings + Hashing
    Implement function to group anagrams using frequency count.

---
+++ Strings/Valid Anagram.py
@@ -0,0 +1,17 @@
+#using frequency approach
+def groupAnagrams(strs):
+  anagrams = {}
+  for word in strs:
+    count = [0]*26
+    for char in word:
+      count[ord(char)-ord('a')] += 1
+    key = tuple(count)
+    if key not in anagrams:
+      anagrams[key] = []
+    anagrams[key].append(word)
+  return list(anagrams.values())
+strs = input("enter the strings: ").aplit()
+print(groupAnagrams(strs))
+
+#Example: strs=["eat", "tea", "ant", "ate", "bat", "tan"]
+#Output: [["eat","tea","ate"], ["ant","tan"], ["bat"]]
